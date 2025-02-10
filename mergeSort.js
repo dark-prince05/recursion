7d39b85c07b826eb6efc87c6a3a17d5c3a798689
Dark-prince <barathraj0504@gmail.com>
2025-02-10 19:28:38 +0530

    mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,9 +1,29 @@
 function mergeSort(arr) {
-  const size = arr.length;
-  let leftArr = arr.splice(0, size / 2);
-  let rightArr = [...arr];
-  console.log(leftArr);
-  console.log(rightArr);
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
-mergeSort([2, 4, 9, 6, 7, 8, 9]);
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+let arr = [3, 2, 1, 13, 8, 5, 0, 1];
+console.log(mergeSort(arr));
